3a2db90ed95d2c5b71bdb21438b1a45e19c6370e
JasmineKim-0418 <gkdmsdl9804@gmail.com>
2021-10-07 00:27:32 +0900

    시작

---
+++ app/src/main/java/com/example/graduation2/CheckActivity.kt
@@ -0,0 +1,11 @@
+package com.example.graduation2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CheckActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_check)
+    }
+}
---
+++ app/src/main/java/com/example/graduation2/Inputnum.kt
@@ -0,0 +1,11 @@
+package com.example.graduation2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Inputnum : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inputnum)
+    }
+}
